b0c85baac21f49c5f2ef4601eb8c730bada4050b
bubblemans <f89647779@gmail.com>
2018-06-26 18:02:22 -0700

    split views

---
+++ DeanzaClassRegister/DeanzaClassRegister/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DeanzaClassRegister
+//
+//  Created by Alvin Lin on 2018/6/26.
+//  Copyright © 2018 Alvin Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
