3a23e8f653da99586bb1bb456cc6e6ccae78868f
bubblemans <f89647779@gmail.com>
2018-08-20 16:29:37 +0800

    clean the code

--- DeanzaClassRegister/DeanzaClassRegister/TableViewController.swift
+++ DeanzaClassRegister/DeanzaClassRegister/TableViewController.swift
@@ -34,18 +34,6 @@ class TableViewController: UITableViewController {
         let course_id: Int?
         let created_at: String?
         let updated_at: String?
-        
-//        init(id: Int? = nil, title: String? = nil, days: String? = nil, times: String? = nil, instructor: String? = nil, location: String? = nil, course_id: Int? = nil, created_at: String? = nil, updated_at: String? = nil) {
-//            self.id = id
-//            self.title = title
-//            self.days = days
-//            self.times = times
-//            self.instructor = instructor
-//            self.location = location
-//            self.course_id = course_id
-//            self.created_at = created_at
-//            self.updated_at = updated_at
-//        }
     }
     
     struct Data: Decodable {
@@ -63,69 +51,51 @@ class TableViewController: UITableViewController {
         let waitlist_slots_capacity: Int?
         let quarter: String?
         var lectures: [Lectures]
-        
-//        init(id: Int? = nil, crn: String? = nil, course: String? = nil, created_at: String? = nil, updated_at: String? = nil, department: String? = nil, status: String? = nil, campus: String? = nil, units: Double? = nil, seats_availible: Int? = nil, waitlist_slots_availible: Int? = nil, waitlist_slots_capacity: Int? = nil, quarter: String? = nil, lectures: [Lectures]) {
-//            self.id = id
-//            self.crn = crn
-//            self.course = course
-//            self.created_at = created_at
-//            self.updated_at = updated_at
-//            self.department = department
-//            self.status = status
-//            self.campus = campus
-//            self.units = units
-//            self.seats_availible = seats_availible
-//            self.waitlist_slots_availible = waitlist_slots_availible
-//            self.waitlist_slots_capacity = waitlist_slots_capacity
-//            self.quarter = quarter
-//            self.lectures = lectures
-//        }
     }
     
     struct Courses: Decodable {
         let total: Int?
         var data: [Data]
-        
-//        init(total: Int? = nil, data: [Data]) {
-//            self.total = total
-//            self.data = data
-//        }
+    }
+    
+    struct Courses2D {
+        var total: Int?
+        var data: [[Data]]
     }
     
     var lectureTest = Lectures(id: 0, title: "", days: "", times: "", instructor: "", location: "", course_id: 0, created_at: "", updated_at: "")
-    lazy var dataTest = Data(id: 0, crn: "", course: "", created_at: "", updated_at: "", department: "", status: "", campus: "", units: 1.0, seats_availible: 0, waitlist_slots_availible: 0, waitlist_slots_capacity: 0, quarter: "", lectures: [lectureTest])
-    lazy var courseTest = Courses(total: 0, data: [dataTest])
+    lazy var dataTest = Data(id: 0, crn: "", course: "", created_at: "", updated_at: "", department: "", status: "", campus: "", units: 0.0, seats_availible: 0, waitlist_slots_availible: 0, waitlist_slots_capacity: 0, quarter: "", lectures: [lectureTest])
+    lazy var courseTest = Courses2D(total: 0, data: [[dataTest]])
     
-    func downloadJson(courseTest: Courses) {
-        let jsonUrlString = "https://api.daclassplanner.com/courses"
+    var departmentList: [String] = []
+    var numOfRowInSect:[Int] = []
+    var numSect = 0
+    var index = 0
+    
+    func downloadJson() {
+        let jsonUrlString = "https://api.daclassplanner.com/courses?sortBy=course"
         guard let url = URL(string: jsonUrlString) else { return }
         
         URLSession.shared.dataTask(with: url) { (data, response, error) in
-            
             guard let data = data else { return }
             
-            //            let dataString = String(data: data, encoding: .utf8)
-            //
-            //            print(dataString as Any)
-            
             do {
-                //                let json = try JSONSerialization.jsonObject(with: data, options: .mutableContainers)
-                //
-                //                print(json)
-                
                 let course = try JSONDecoder().decode(Courses.self, from: data)
+                var index = 0
+                var temp: [Data] = []
+                self.courseTest.data.remove(at: 0)
                 
-                //                guard let total = Courses.total else { return }
-                //                print(course.total!)
+                while index < course.total! - 1{
+                    temp.append(course.data[index])
+                    if course.data[index].department != course.data[index + 1].department {
+                        self.departmentList.append(course.data[index].department!)
+                        self.courseTest.data.append(temp)
+                        temp = []
+                    }
+                    index = index + 1
+                }
+                self.courseTest.total = course.total
                 
-//                var index = 0
-//                for _ in course.data {
-//                    print(course.data[index].course!, "    ",course.data[index].lectures[0].title!, "    ", course.data[index].lectures[0].instructor!)
-//
-//                    index = index + 1
-//
-//                }
-                self.courseTest = course
                 DispatchQueue.main.async {
                     self.tableView.reloadData()
                 }
@@ -138,21 +108,12 @@ class TableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+        
         navigationItem.title = "Classes"
         navigationController?.navigationBar.prefersLargeTitles = true
         
-        
-        
-        downloadJson(courseTest: courseTest)
-//        self.tableView.reloadData()
-        print("done")
-        
+        downloadJson()
     }
 
     override func didReceiveMemoryWarning() {
@@ -162,14 +123,15 @@ class TableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-//    override func numberOfSections(in tableView: UITableView) -> Int {
-//        // #warning Incomplete implementation, return the number of sections
-//        return testArray.count
-//    }
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return courseTest.data.count
+    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return courseTest.total!
+        
+        return courseTest.data[section].count
     }
 
     
@@ -177,33 +139,36 @@ class TableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
 
         // Configure the cell...
-//        cell.textLabel?.text = testArray[indexPath.section][indexPath.row]
         let blank = "    "
         
         
-//        let text = course + blank + instructor + blank + status
-        
         var status: String
-        if courseTest.data[indexPath.row].status != nil {
-            status = courseTest.data[indexPath.row].status!
-        }
-        else {
+        if courseTest.data[indexPath.section][indexPath.row].status != nil {
+            status = courseTest.data[indexPath.section][indexPath.row].status!
+        } else {
             status = "nil"
         }
         
-        let text = courseTest.data[indexPath.row].crn! + blank + courseTest.data[indexPath.row].course! + blank +  courseTest.data[indexPath.row].lectures[0].instructor! + blank + status
+        let text = courseTest.data[indexPath.section][indexPath.row].crn! + blank + courseTest.data[indexPath.section][indexPath.row].course! + blank +  courseTest.data[indexPath.section][indexPath.row].lectures[0].instructor! + blank + status
         
         cell.textLabel?.text = text
+        
         return cell
     }
     
-//    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-//        let label = UILabel()
-//        label.text = sectionTitle[section]
-//        label.backgroundColor = UIColor.lightGray
-//
-//        return label
-//    }
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let label = UILabel()
+       
+        if departmentList.count != 0 {
+            label.text = departmentList[section]
+        } else {
+            return label
+        }
+        
+        label.backgroundColor = UIColor.lightGray
+
+        return label
+    }
     
 //    override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
 //        return indexTitle
