1f4a85843b49550a57789479b4b95379895c3bf3
bubblemans <f89647779@gmail.com>
2018-09-28 19:10:47 -0700

    favoriteList view configuration

--- DeanzaClassRegister/DeanzaClassRegister/Controller/MenuViewControllers/NotificationViewController.swift
+++ DeanzaClassRegister/DeanzaClassRegister/Controller/MenuViewControllers/NotificationViewController.swift
@@ -8,29 +8,19 @@
 
 import UIKit
 
-class NotificationViewController: MenuBaseViewController, UITableViewDelegate, UITableViewDataSource {
+class NotificationViewController: MenuBaseViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
-    let cellId = "cellId"
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
-    }
-
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
-        return cell
-    }
-    
-    let tableView: UITableView = {
-        let view = UITableView()
-        view.backgroundColor = .black
+    let collectionView: UICollectionView = {
+        let view = UICollectionView()
         return view
     }()
     
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.addSubview(tableView)
-    
+        collectionView.dataSource = self
+        collectionView.delegate = self
     }
 }
 
