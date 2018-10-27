85b4d223d26e54d4caf5ca481e211bfab51b1822
bubblemans <f89647779@gmail.com>
2018-10-27 14:56:45 -0700

    delete extension of label(underline)

--- DeanzaClassRegister/DeanzaClassRegister/LoginSystem/SignUpVC.swift
+++ DeanzaClassRegister/DeanzaClassRegister/LoginSystem/SignUpVC.swift
@@ -13,7 +13,7 @@ class SignUpVC: UIViewController {
     // Username Textfield
     let usernameTextfield : UITextField = {
         let userTextfield = UITextField()
-        userTextfield.attributedPlaceholder = NSAttributedString(string:"Username(Email): ",attributes: [NSAttributedString.Key.foregroundColor: UIColor.gray])
+        userTextfield.attributedPlaceholder = NSAttributedString(string:"  Username ",attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
         userTextfield.backgroundColor = #colorLiteral(red: 0.8470588235, green: 0.8470588235, blue: 0.8470588235, alpha: 0.5)
         userTextfield.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         userTextfield.borderStyle = UITextField.BorderStyle.roundedRect
@@ -21,12 +21,13 @@ class SignUpVC: UIViewController {
         userTextfield.keyboardType = UIKeyboardType.default
         userTextfield.returnKeyType = UIReturnKeyType.done
         userTextfield.clearButtonMode = UITextField.ViewMode.whileEditing
+        userTextfield.autocapitalizationType = .none
         return userTextfield
     } ()
     
     let passwordTextfield : UITextField = {
         let passTextfield = UITextField()
-        passTextfield.attributedPlaceholder = NSAttributedString(string:"Password: ", attributes: [NSAttributedString.Key.foregroundColor: UIColor.gray])
+        passTextfield.attributedPlaceholder = NSAttributedString(string:"  Password ", attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
         passTextfield.backgroundColor = #colorLiteral(red: 0.8470588235, green: 0.8470588235, blue: 0.8470588235, alpha: 0.5)
         passTextfield.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
         passTextfield.borderStyle = UITextField.BorderStyle.roundedRect
@@ -34,16 +35,34 @@ class SignUpVC: UIViewController {
         passTextfield.keyboardType = UIKeyboardType.default
         passTextfield.returnKeyType = UIReturnKeyType.done
         passTextfield.clearButtonMode = UITextField.ViewMode.whileEditing
+        passTextfield.autocapitalizationType = .none
+        passTextfield.isSecureTextEntry = true
         return passTextfield
     }()
     
-    let signupButton2 : UIButton = {
-        let signupBtn2 = UIButton()
-        signupBtn2.setTitle("Done", for: .normal)
-        signupBtn2.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
-        signupBtn2.setTitleColor(#colorLiteral(red: 0.4078431373, green: 0.007843137255, blue: 0.1490196078, alpha: 1), for: .normal)
-        return signupBtn2
+    let phoneTextField: UITextField = {
+        let view = UITextField()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.autocapitalizationType = .none
+        view.autocorrectionType = .no
+        return view
+    }()
+    
+    
+    
+    let signUpButton : UIButton = {
+        let bt = UIButton()
+        bt.setTitle("Done", for: .normal)
+        bt.backgroundColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        bt.setTitleColor(#colorLiteral(red: 0.4078431373, green: 0.007843137255, blue: 0.1490196078, alpha: 1), for: .normal)
+        return bt
         
+    }()
+    
+    let backButton: UIButton = {
+        let bt = UIButton()
+        bt.translatesAutoresizingMaskIntoConstraints = false
+        return bt
     }()
 
     let imageView : UIImageView = {
@@ -80,8 +99,9 @@ class SignUpVC: UIViewController {
         setProfileImageView()
         setupUserTextfield()
         setupPassTextfield()
-        setSignupBtn2()
-        setUploadButton()
+        setupSignupBt()
+        setUpAddPicButton()
+        setupBackButton()
         
     }
     
@@ -109,15 +129,24 @@ class SignUpVC: UIViewController {
         passwordTextfield.heightAnchor.constraint(equalToConstant: 40).isActive = true
     }
     
-    private func setSignupBtn2(){
+    private func setupSignupBt(){
         let height = CGFloat(40)
-        self.view.addSubview(signupButton2)
-        signupButton2.translatesAutoresizingMaskIntoConstraints = false
-        signupButton2.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
-        signupButton2.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 550).isActive = true
-        signupButton2.widthAnchor.constraint(equalToConstant: 274).isActive = true
-        signupButton2.heightAnchor.constraint(equalToConstant: height).isActive = true
-        signupButton2.layer.cornerRadius = height / 2
+        self.view.addSubview(signUpButton)
+        signUpButton.translatesAutoresizingMaskIntoConstraints = false
+        signUpButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        signUpButton.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 550).isActive = true
+        signUpButton.widthAnchor.constraint(equalToConstant: 274).isActive = true
+        signUpButton.heightAnchor.constraint(equalToConstant: height).isActive = true
+        signUpButton.layer.cornerRadius = height / 2
+    }
+    
+    private func setupBackButton() {
+        self.view.addSubview(backButton)
+        backButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 550).isActive = true
+        backButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        backButton.widthAnchor.constraint(equalToConstant: 274).isActive = true
+        backButton.heightAnchor.constraint(equalTo: view.heightAnchor).isActive = true
+        
     }
     
     private func setProfileImageView(){
@@ -130,14 +159,17 @@ class SignUpVC: UIViewController {
         imageView.heightAnchor.constraint(equalToConstant:150).isActive = true
     }
     
-    private func setUploadButton(){
+    private func setUpAddPicButton(){
         self.view.addSubview(addPicButton)
         addPicButton.translatesAutoresizingMaskIntoConstraints = false
         addPicButton.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 230).isActive = true
         addPicButton.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 260).isActive = true
         addPicButton.widthAnchor.constraint(equalToConstant: 50).isActive = true
         addPicButton.heightAnchor.constraint(equalToConstant:50).isActive = true
-        
-        
     }
 }
+
+
+
+
+
